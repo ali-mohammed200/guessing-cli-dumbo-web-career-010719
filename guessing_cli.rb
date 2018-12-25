6b4383cac83ab54fe1d58daa4b6683ee436f4496
Mohammad Ali <ali.mohammed200@gmail.com>
2018-12-25 01:31:48 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   puts "Guess a number between 1 and 6."
   randNumb = rand(1...6)
   userInp = gets.chomp
-  if userInp == randNumb.to_s
+  if userInp == randNumb
     puts "You guessed the correct number!"
   elsif userInp == "exit"
     puts "Goodbye!"
