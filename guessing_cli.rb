d8eef8805494c4cb108cc09ca13d48db53700670
Mohammad Ali <ali.mohammed200@gmail.com>
2018-12-25 01:31:10 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,7 +1,7 @@
 # Code your solution here!
 def run_guessing_game
   puts "Guess a number between 1 and 6."
-  randNumb = rand(1...6)
+  randNumb = rand(1...6).to_s
   userInp = gets.chomp
   if userInp == randNumb
     puts "You guessed the correct number!"
